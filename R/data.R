785a8f69ffe5e00baeb64510d1ed70399a9ee0f4
Rafael Garcia <rafael.garcia@ericsson.com>
2018-02-09 12:40:12 +0100

    Adding earthquake data.frame as package data

---
+++ R/data.R
@@ -0,0 +1,16 @@
+#' Significant Earthquake Database
+#'
+#' Significant Earthquake Database. National Geophysical Data Center, NOAA. doi:10.7289/V5TD9V7K
+#'
+#' The Significant Earthquake Database contains information on destructive earthquakes from 2150 B.C.
+#' to the present that meet at least one of the following criteria: Moderate damage (approximately $1 million or more),
+#' 10 or more deaths, Magnitude 7.5 or greater, Modified Mercalli Intensity X or greater, or the earthquake generated a tsunami.
+#'
+#' @source Significant Earthquake Database. National Geophysical Data Center, NOAA. doi:10.7289/V5TD9V7K.
+#' \url{https://www.ngdc.noaa.gov/nndc/struts/form?t=101650&s=1&d=1}
+#' @format A data frame with columns specified in \url{https://www.ngdc.noaa.gov/nndc/struts/results?&t=101650&s=225&d=225}
+#' @examples
+#' \dontrun{
+#'  earthquake_data_raw
+#' }
+"earthquake_data_raw"
